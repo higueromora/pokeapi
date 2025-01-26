2824c2737004a35a3ff9e7d9c4691b11fda6746e
higueromora <higueromora@hotmail.com>
2025-01-26 22:52:21 +0100

    a

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -7,71 +7,75 @@ export const usePokemon = (search, page) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchPokemon = async () => {
-      try {
-        let pokemon_data = [];
+    const timeoutId = setTimeout(() => {
+      const fetchPokemon = async () => {
+        try {
+          let pokemon_data = [];
 
-        for (let i = page[0]; i <= page[1]; i++) {
-          const res = await fetch(`${END_POINT_GET_POKEMON}${i}`);
+          for (let i = page[0]; i <= page[1]; i++) {
+            const res = await fetch(`${END_POINT_GET_POKEMON}${i}`);
+            if (!res.ok) {
+              throw new Error("Pokémon not found");
+            }
+            const data = await res.json();
+            const { name, id, types, sprites } = data;
+            pokemon_data.push({
+              name,
+              id,
+              types: types.map((typeInfo) => typeInfo.type.name),
+              sprite: sprites.front_default,
+            });
+          }
+
+          setError(null);
+          setPokemon(pokemon_data);
+        } catch (err) {
+          console.log(err);
+          setPokemon([]);
+          setError("Pokémon not found");
+        }
+      };
+
+      const fetchSearchPokemon = async () => {
+        try {
+          let url = "";
+          if (search.number) {
+            url = `${END_POINT_GET_POKEMON}${search.number}`;
+          } else if (search.name) {
+            url = `${END_POINT_GET_POKEMON}${search.name.toLowerCase()}`;
+          }
+
+          const res = await fetch(url);
           if (!res.ok) {
             throw new Error("Pokémon not found");
           }
+
           const data = await res.json();
           const { name, id, types, sprites } = data;
-          pokemon_data.push({
-            name,
-            id,
-            types: types.map((typeInfo) => typeInfo.type.name),
-            sprite: sprites.front_default,
-          });
+          setError(null);
+          setPokemon([
+            {
+              name,
+              id,
+              types: types.map((typeInfo) => typeInfo.type.name),
+              sprite: sprites.front_default,
+            },
+          ]);
+        } catch (err) {
+          console.error(err);
+          setPokemon([]);
+          setError("Pokémon not found");
         }
+      };
 
-        setError(null);
-        setPokemon(pokemon_data);
-      } catch (err) {
-        console.log(err);
-        setPokemon([]);
-        setError("Pokémon not found");
+      if (!search.number && !search.name) {
+        fetchPokemon();
+      } else {
+        fetchSearchPokemon();
       }
-    };
+    }, 500); // Espera medio segundo antes de ejecutar la búsqueda.
 
-    const fetchSearchPokemon = async () => {
-      try {
-        let url = "";
-        if (search.number) {
-          url = `${END_POINT_GET_POKEMON}${search.number}`;
-        } else if (search.name) {
-          url = `${END_POINT_GET_POKEMON}${search.name.toLowerCase()}`;
-        }
-
-        const res = await fetch(url);
-        if (!res.ok) {
-          throw new Error("Pokémon not found");
-        }
-
-        const data = await res.json();
-        const { name, id, types, sprites } = data;
-        setError(null);
-        setPokemon([
-          {
-            name,
-            id,
-            types: types.map((typeInfo) => typeInfo.type.name),
-            sprite: sprites.front_default,
-          },
-        ]);
-      } catch (err) {
-        console.error(err);
-        setPokemon([]);
-        setError("Pokémon not found");
-      }
-    };
-
-    if (!search.number && !search.name) {
-      fetchPokemon();
-    } else {
-      fetchSearchPokemon();
-    }
+    return () => clearTimeout(timeoutId); // Limpia el timeout para evitar llamadas innecesarias.
   }, [page, search]);
 
   return { pokemon, error };
